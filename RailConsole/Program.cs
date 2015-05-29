a1681e593a6cc922369d392de201a4a6e9391058
Ros Haitovich <ros.haitovich@kerensen.com>
2015-05-29 09:55:39 +0300

    init

---
+++ RailConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RailConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rail application testing.......");
+            Console.ReadKey();
+        }
+    }
+}
