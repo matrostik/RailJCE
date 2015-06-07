53e88ffa6d9da0aa2081d5e7731bb6b78eed9d60
Ros Haitovich <ros.haitovich@kerensen.com>
2015-06-07 19:47:16 +0300

    dal refactoring + win forms int

--- RailConsole/Program.cs
+++ RailConsole/Program.cs
@@ -19,7 +19,9 @@ namespace RailConsole
             RailDAL dal = new RailDAL();
             var list1 = dal.GetAllStations();
             var list2 = dal.GetAllRoutes();
-
+            var list3 = dal.GetLineStations("A");
+            var list4 = dal.GetLineStations(15);
+            var flag = dal.OnSameLine(5, 10);
 
             foreach (var item in list1)
             {
--- RailDomain/RailDAL.cs
+++ RailDomain/RailDAL.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RailDomain.Helpers;
+using System.Data;
 
 namespace RailDomain
 {
@@ -18,37 +19,85 @@ namespace RailDomain
             conn = new SqlConnection(conStr);
         }
 
-        private string conStr{get;set;}
+        private string conStr { get; set; }
         private SqlConnection conn { get; set; }
 
-
+        /// <summary>
+        /// Get all stations
+        /// </summary>
         public List<Station> GetAllStations()
         {
-            List<Station> stations;
-            conn.Open();
-            SqlCommand command = new SqlCommand("SELECT * FROM Stations", conn);
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                stations = reader.MapDataToModel<Station>();
-            }
-            conn.Close();
-
-            return stations;
+            string query = "SELECT * FROM [Stations]";
+            return ExecuteQuery<Station>(query);
         }
 
+        /// <summary>
+        /// Get all routes
+        /// </summary>
         public List<Route> GetAllRoutes()
         {
-            List<Route> routes;
-            conn.Open();
-            SqlCommand command = new SqlCommand("SELECT * FROM Routes", conn);
-            using (SqlDataReader reader = command.ExecuteReader())
-            {
-                routes = reader.MapDataToModel<Route>();
-            }
-            conn.Close();
-
-            return routes;
+            string query = "SELECT * FROM [Routes]";
+            return ExecuteQuery<Route>(query);
         }
 
+        /// <summary>
+        /// Get all stations an selected line
+        /// </summary>
+        /// <param name="line">line name</param>
+        public List<Station> GetLineStations(string line)
+        {
+            string query = string.Format("SELECT * FROM [Stations] WHERE Id IN (SELECT StationId FROM [Routes] WHERE Line='{0}')", line);
+            return ExecuteQuery<Station>(query);
+        }
+
+        /// <summary>
+        /// Get all stations that can be reached from selected station
+        /// </summary>
+        /// <param name="stationId">station id</param>
+        public List<Station> GetLineStations(int stationId)
+        {
+            // must return stations????
+            string query = string.Format("SELECT * FROM [Stations] WHERE Id IN (SELECT StationId FROM [Routes] WHERE Line IN (SELECT Line FROM [Routes] WHERE StationId={0}))", stationId);
+            return ExecuteQuery<Station>(query);
+        }
+
+        /// <summary>
+        /// Check if source and destination stations on the same line.
+        /// </summary>
+        /// <param name="source">source station id</param>
+        /// <param name="destination">destination station id</param>
+        public bool OnSameLine(int source, int destination)
+        {
+            string query = string.Format("SELECT a.LINE FROM [Routes] a, [Routes] b WHERE a.StationId={0} AND b.StationId={1} AND a.Line=b.Line", source, destination);
+            List<Route> routes = ExecuteQuery<Route>(query);
+            // not good
+            return routes.Count > 0;
+        }
+
+        /// <summary>
+        /// ExecuteQuery and return list of objects
+        /// </summary>
+        /// <typeparam name="T">Type</typeparam>
+        /// <param name="query">Query string</param>
+        private List<T> ExecuteQuery<T>(string query) where T : new()
+        {
+            List<T> result = new List<T>();
+            try
+            {
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    result = reader.MapDataToModel<T>();
+                }
+                conn.Close();
+                return result;
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                return result;
+            }
+        }
     }
 }
